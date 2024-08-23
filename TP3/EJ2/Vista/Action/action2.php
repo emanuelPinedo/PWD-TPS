9f88449d844062900f65f01fd3c1cf7f4a337f5f
Federico <fedee.unates.2001@gmail.com>
2024-08-23 16:17:20 -0300

    tp3 ej2, arreglado y con bootstrap

--- TP3/EJ2/Vista/Action/action2.php
+++ TP3/EJ2/Vista/Action/action2.php
@@ -5,33 +5,36 @@ include_once "../../Utils/datasubmited.php";
 
 $datos = dataSubmitted();
 
-$archivoSubir = new Archivo(); 
+$archivoSubir = new Archivo();
 $rta = $archivoSubir->subirArchivo($datos);
 
 
-if ($rta == 0){
-    $texto = "<p>ERROR: no se cargó el archivo </p>";
-} 
-elseif ($rta == 1){
-    $texto = "<textarea rows='10' cols='50'>".file_get_contents($archivo->getDir() . $datos['miArchivo']['name'])."</textarea>";
-}
-elseif($rta == -1){
-    $texto = "<p>ERROR: no se pudo cargar el archivo. No se pudo acceder al archivo Temporal</p>";
-}
- else {
-    $texto = "<p>ERROR: no se pudo cargar el archivo. Asegurese de que sea un archivo txt</p>";
+if ($rta == 0) {
+  $texto = "<p>ERROR: no se cargó el archivo </p>";
+} elseif ($rta == 1) {
+  $texto = "<textarea class='p-3 bg-secondary text-light' rows='10' cols='50'>" . file_get_contents($archivoSubir->getDir() . $datos['miArchivo']['name']) . "</textarea>";
+} elseif ($rta == -1) {
+  $texto = "<p class='text-danger border-bottom border-danger'>ERROR: no se pudo cargar el archivo. No se pudo acceder al archivo Temporal</p>";
+} else {
+  $texto = "<p class='text-danger border-bottom border-danger'>ERROR: no se pudo cargar el archivo. Asegurese de que sea un archivo txt</p>";
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Archivo</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <title>Archivo</title>
 </head>
-<body> 
-    <div><h1><?php echo $texto; ?></h1></div>
-    <a href="http://localhost/PWD-TPS/TP3/EJ2/Vista/ej2.php">Volver</a>
+
+<body class="text-light bg-dark d-flex align-items-center justify-content-center flex-column mt-5">
+  <div>
+    <h1><?php echo $texto; ?></h1>
+  </div>
+  <a class="p-1 pl-3 pr-3 bg-light border rounded border-secondary" href="http://localhost/PWD-TPS/TP3/EJ2/Vista/ej2.php">Volver</a>
 </body>
+
 </html>
--- TP3/EJ2/Vista/ej2.php
+++ TP3/EJ2/Vista/ej2.php
@@ -1,16 +1,20 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Subir Archivo</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <title>Subir Archivo</title>
 </head>
-<body>
-    <h2>Subir Archivo</h2>
-    <form action="./Action//action2.php" method="post" enctype="multipart/form-data">
-        <label for="miArchivo">Elige un archivo (.txt):</label><br><br>
-        <input type="file" name="miArchivo" id="miArchivo"><br><br>
-        <input type="submit" value="Subir Archivo">
-    </form>
+
+<body class="text-light bg-dark d-flex align-items-center justify-content-center flex-column mt-5">
+  <h2>Subir Archivo</h2>
+  <form action="./Action//action2.php" method="post" enctype="multipart/form-data">
+    <label for="miArchivo">Elige un archivo (.txt):</label><br><br>
+    <input type="file" name="miArchivo" id="miArchivo" class="border rounded border-secondary p-1"><br><br>
+    <input type="submit" class="bg-info w-100 border rounded border-secondary" value="Subir Archivo">
+  </form>
 </body>
+
 </html>
