f22a674b6fe778e744433124850e60caeb1d6744
Federico <fedee.unates.2001@gmail.com>
2024-09-12 20:22:43 -0300

    formulario para nueva persona con bootstrap

--- TP4/Vista/NuevaPersona.php
+++ TP4/Vista/NuevaPersona.php
@@ -18,38 +18,53 @@ $tp3ej3 = "../../TP3/EJ3/Vista/ej3.php";
 include_once('../Vista/Estructura/header.php');
 //este tp fue hecho con 3 acompañantes llamados 3 latas de speed
 ?>
-<h1>Consigna</h1>
-<p>
-  – Crear una página “NuevaPersona.php” que contenga un formulario que permita solicitar todos
-  los datos de una persona. Estos datos serán enviados a una página “accionNuevaPersona.php” que cargue
-  un nuevo registro en la tabla persona de la base de datos. Se debe mostrar un mensaje que indique si se
-  pudo o no cargar los datos de la persona. Utilizar css y validaciones javaScript cuando crea conveniente.
-  Recordar usar la capa de control antes generada, no se puede acceder directamente a las clases del ORM.
-</p>
+<main class="pl-5 pr-5">
+  <div class="d-flex mt-2">
 
-<h1>Registrar Nueva Persona</h1>
-<form action="../Vista/Action/actionNuevaPersona.php" method="post">
-  <div id="error" class="error"></div>
-  <label for="NroDni">DNI:</label>
-  <input type="number" id="NroDni" name="NroDni">
+    <h1 class="mr-3 mt-2 text-primary">Consigna</h1>
 
-  <label for="Apellido">Apellido:</label>
-  <input type="text" id="Apellido" name="Apellido">
+    <p class="ml-3 mt-2">
+      – Crear una página “NuevaPersona.php” que contenga un formulario que permita solicitar todos
+      los datos de una persona. Estos datos serán enviados a una página “accionNuevaPersona.php” que cargue
+      un nuevo registro en la tabla persona de la base de datos. Se debe mostrar un mensaje que indique si se
+      pudo o no cargar los datos de la persona. Utilizar css y validaciones javaScript cuando crea conveniente.
+      Recordar usar la capa de control antes generada, no se puede acceder directamente a las clases del ORM.
+    </p>
+  </div>
+  <div class="d-flex flex-column align-items-center justify-content-center mb-5">
 
-  <label for="Nombre">Nombre:</label>
-  <input type="text" id="Nombre" name="Nombre">
+    <h1 class="text-primary">Registrar Nueva Persona</h1>
+    <form class="w-25 bg-light shadow p-3" action="../Vista/Action/actionNuevaPersona.php" method="post">
+      <div id="error" class="error"></div>
+      <div class="w-100 d-flex flex-column">
+        <label for="NroDni">DNI:</label>
+        <input type="number" id="NroDni" name="NroDni">
+      </div>
+      <div class="w-100 d-flex flex-column">
+        <label for="Apellido">Apellido:</label>
+        <input type="text" id="Apellido" name="Apellido">
+      </div>
+      <div class="w-100 d-flex flex-column">
+        <label for="Nombre">Nombre:</label>
+        <input type="text" id="Nombre" name="Nombre">
+      </div>
+      <div class="w-100 d-flex flex-column">
+        <label for="fechaNac">Fecha de Nacimiento:</label>
+        <input type="date" id="fechaNac" name="fechaNac">
+      </div>
+      <div class="w-100 d-flex flex-column">
+        <label for="Telefono">Teléfono:</label>
+        <input type="tel" id="Telefono" name="Telefono">
+      </div>
+      <div class="w-100 d-flex flex-column">
+        <label for="Domicilio">Domicilio:</label>
+        <input type="text" id="Domicilio" name="Domicilio">
+      </div>
 
-  <label for="fechaNac">Fecha de Nacimiento:</label>
-  <input type="date" id="fechaNac" name="fechaNac">
-
-  <label for="Telefono">Teléfono:</label>
-  <input type="tel" id="Telefono" name="Telefono">
-
-  <label for="Domicilio">Domicilio:</label>
-  <input type="text" id="Domicilio" name="Domicilio">
-
-  <input type="submit" value="Registrar">
-</form>
+      <input class="w-100 btn btn-primary mt-3" type="submit" value="Registrar">
+    </form>
+  </div>
+</main>
 <?php
 include_once('../Vista/Estructura/footer.php');
 ?>
