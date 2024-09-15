8499c5b109de2167cba2f873df3a41bf24368bab
Federico <fedee.unates.2001@gmail.com>
2024-09-15 04:31:40 -0300

    pequeño arreglo en el link para volver al formulario

--- TP4/Vista/Action/actionBuscarPersona.php
+++ TP4/Vista/Action/actionBuscarPersona.php
@@ -1,5 +1,5 @@
 <?php
-include_once ('../../config.php');
+include_once('../../config.php');
 include_once('../Estructura/header.php');
 $datos = data_submitted();
 $dni = $datos['NroDni'];
@@ -9,44 +9,47 @@ $objABMPersona = new ABMPersona();
 $persona = $objABMPersona->buscar(['NroDni' => $dni]);
 
 if (count($persona) > 0) {
-    $persona = $persona[0];
-    ?>
-    <!DOCTYPE html>
-    <html lang="es">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    </head>
-    <body>
-        <h1>Datos persona</h1>
-        <form action="ActualizarDatosPersona.php" method="post">
-            <input type="hidden" name="NroDni" value="<?php echo htmlspecialchars($persona['NroDni']); ?>">
+  $persona = $persona[0];
+?>
+  <!DOCTYPE html>
+  <html lang="es">
 
-            <label for="Apellido">Apellido:</label>
-            <input type="text" id="Apellido" name="Apellido" value="<?php echo htmlspecialchars($persona['Apellido']); ?>">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
 
-            <label for="Nombre">Nombre: </label>
-            <input type="text" name="Nombre" id="Nombre" value="<?php echo htmlspecialchars($persona['Nombre']); ?>">
+  <body>
+    <h1>Datos persona</h1>
+    <form action="ActualizarDatosPersona.php" method="post">
+      <input type="hidden" name="NroDni" value="<?php echo htmlspecialchars($persona['NroDni']); ?>">
 
-            <label for="fechaNac">Fecha de Nacimiento:</label>
-            <input type="text" id="fechaNac" name="fechaNac" value="<?php echo htmlspecialchars($persona['fechaNac']); ?>">
+      <label for="Apellido">Apellido:</label>
+      <input type="text" id="Apellido" name="Apellido" value="<?php echo htmlspecialchars($persona['Apellido']); ?>">
 
-            <label for="Telefono">Teléfono:</label>
-            <input type="text" id="Telefono" name="Telefono" value="<?php echo htmlspecialchars($persona['Telefono']); ?>">
+      <label for="Nombre">Nombre: </label>
+      <input type="text" name="Nombre" id="Nombre" value="<?php echo htmlspecialchars($persona['Nombre']); ?>">
 
-            <label for="Domicilio">Domicilio:</label>
-            <input type="text" id="Domicilio" name="Domicilio" value="<?php echo htmlspecialchars($persona['Domicilio']); ?>">
+      <label for="fechaNac">Fecha de Nacimiento:</label>
+      <input type="text" id="fechaNac" name="fechaNac" value="<?php echo htmlspecialchars($persona['fechaNac']); ?>">
 
-            <input type="submit" value="Actualizar">
-            <br>
-            <a href="http://localhost/PWD-TPS/TP4/Vista/BuscarPersona.php#">Volver</a>
-        </form>
-    </body>
-    </html>
-    <?php
+      <label for="Telefono">Teléfono:</label>
+      <input type="text" id="Telefono" name="Telefono" value="<?php echo htmlspecialchars($persona['Telefono']); ?>">
+
+      <label for="Domicilio">Domicilio:</label>
+      <input type="text" id="Domicilio" name="Domicilio" value="<?php echo htmlspecialchars($persona['Domicilio']); ?>">
+
+      <input type="submit" value="Actualizar">
+      <br>
+      <a href="http://localhost/PWD-TPS/TP4/Vista/BuscarPersona.php#">Volver</a>
+    </form>
+  </body>
+
+  </html>
+<?php
 } else {
-    echo "<p>No hay una persona con este DNI, o no ha sido encontrada.</p>";
+  echo "<p>No hay una persona con este DNI, o no ha sido encontrada.</p>";
 }
 ?>
-<a href="http://localhost/PWD-TPS/TP4/Vista/buscarAutos.php#">Volver</a>
+<a href="http://localhost/PWD-TPS/TP4/Vista/BuscarPersona.php">Volver</a>
 <?php include_once("../Estructura/footer.php"); ?>
